e7508e0e11c2f9c5d2c47d350de53ef5767ce6be
平松大悟朗 <h.dai56@gmail.com>
2020-05-22 23:02:40 +0900

    section12 236

--- store/places-actions.js
+++ store/places-actions.js
@@ -2,9 +2,13 @@ import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACE = "ADD_PLACE";
 
+import { insertPlace } from "../helpers/db";
+
 export const addPlace = (title, image) => {
   return async dispatch => {
     const fileName = image.split('/').pop();
+    const a = image;
+    const b = image.split('/');
     const newPath = FileSystem.documentDirectory + fileName;
 
     try{
@@ -12,17 +16,27 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy address",
+        12.2,
+        16.6
+      );
+      console.log(dbResult,a,"aaaaa",b,fileName);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { 
+          id: dbResult.insertId,
+          title: title,
+          image: newPath
+         }
+      });
     } catch (e) {
       console.log(e);
       throw e;
     }
 
-    dispatch({
-      type: ADD_PLACE,
-      placeData: { 
-        title: title,
-        image: newPath
-       }
-    });
+
   };
 };
