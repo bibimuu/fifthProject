3dc550ba468cfcbab230bb29aed9c04a4fa94649
平松大悟朗 <h.dai56@gmail.com>
2020-06-02 21:40:50 +0900

    section14 completed about config

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -53,7 +53,7 @@ const NewPlaceScreen = props => {
 };
 
 NewPlaceScreen.navigationOptions = {
-    headerTitle: "Add Place"
+    headerTitle: "Add New Place"
 };
 
 const styles = StyleSheet.create({
